aa3d8adcc80b5958debcd986d27d2df5f4ad7ee9
Ian Stevens <istevens@null.net>
2024-08-22 11:32:11 -0400

    Added detection of pairs, trios, and quads. Moved cards map to member of hands as hands is not technically a map

--- src/cribbagescorer.js
+++ src/cribbagescorer.js
@@ -4,25 +4,42 @@ const JACK = 'J';
 const FACES = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', JACK, 'Q', 'K');
 const SUITS = ('D', 'C', 'H', 'S');
 
-class CribbageHand extends Array {
+const extendExistingOrCreate = (m,v) => m.set(v[0], (m.get(v[0]) || []).concat(v[1]));
+
+class CribbageHand {
+
+    constructor(cards) {
+        cards = cards.map((x) => [x[0], [x[1]]])
+        this.cards = cards.reduce(extendExistingOrCreate, new Map());
+    }
 
     hasHisNobs(cutCard) {
-        const cutSuit = cutCard[1];
-        var jackOfCutSuitIsInHand = this.some((x) => x[0] == JACK && x[1] == cutSuit);
+        var cutSuit = cutCard[1];
+        var jacks = this.cards.get(JACK) || false;
+        var jackOfCutSuitIsInHand = jacks && jacks.indexOf(cutSuit) >= 0;
         return jackOfCutSuitIsInHand;
     }
 
     hasFlush() {
-        var suits = this.map((x) => x[1]);
-        suits = suits.reduce((x,y) => x == y && y);
-        var hasFlush = suits != false;
+        var suits = Array.from(this.cards.values()).flat();
+        suits = new Set(suits).size;
+        var hasFlush = suits == 1;
         return hasFlush;
+    }
+
+    findMultiples(cutCard) {
+        var hand = new Map(this.cards);
+        extendExistingOrCreate(hand, cutCard);
+        hand = Array.from(hand);
+        var multiples = hand.filter((x) => x[1].length > 1);
+        multiples = multiples.map((x) => x[1].map((y) => x[0] + y));
+        return multiples;
     }
 }
 
 export function handFromString(s) {
     var hand = s.split(' ');
     hand = hand.map((x) => Array.from(x));
-    hand = CribbageHand.from(hand);
+    hand = new CribbageHand(hand);
     return hand;
 }
--- src/cribbagescorer.test.js
+++ src/cribbagescorer.test.js
@@ -46,3 +46,70 @@ describe('testing Hand.hasFlush', () => {
         expectHasFlush(hand, false);
     });
 });
+
+describe('testing Hand.findMultiples', () => {
+
+    const findMultiples = (p) => {
+        var [h, c] = p;
+        h = handFromString(h);
+        var multiples = h.findMultiples(c);
+        return multiples;
+    }
+
+    const expectMultiplesToBe = (p, e) => {
+        var m = findMultiples(p);
+        expect(m).toEqual(e);
+    }
+
+    test('returns only one pair if in hand', () => {
+        var play = ["AC 2C 3C 3D", "5D"];
+        expectMultiplesToBe(play, [["3C", "3D"]]);
+    });
+
+    test('returns only one pair if in hand and cut card', () => {
+        var play = ["AC 2C 3C 4D", "3D"];
+        expectMultiplesToBe(play, [["3C", "3D"]]);
+    });
+
+    test('returns two pair if in hand', () => {
+        var play = ["3C 2C 3D 2D", "5D"];
+        var m = findMultiples(play);
+        expect(m).toContainEqual(["2C", "2D"]);
+        expect(m).toContainEqual(["3C", "3D"]);
+    });
+
+    test('returns two pair if in hand and cut card', () => {
+        var play = ["3C 2C 5D 2D", "3D"];
+        var m = findMultiples(play);
+        expect(m).toContainEqual(["2C", "2D"]);
+        expect(m).toContainEqual(["3C", "3D"]);
+    });
+
+    test('returns only one trio if in hand', () => {
+        var play = ["AC 3H 3C 3D", "5D"];
+        expectMultiplesToBe(play, [["3H", "3C", "3D"]]);
+    });
+
+    test('returns only one trio if in hand and cut card', () => {
+        var play = ["AC 2H 3C 3D", "3S"];
+        expectMultiplesToBe(play, [["3C", "3D", "3S"]]);
+    });
+
+    test('returns trio and pair if in hand and cut card', () => {
+        var play = ["2C 2H 3C 3D", "3S"];
+        var m = findMultiples(play);
+        expect(m).toContainEqual(["2C", "2H"]);
+        expect(m).toContainEqual(["3C", "3D", "3S"]);
+    });
+
+    test('returns only one quad if in hand', () => {
+        var play = ["3S 3H 3C 3D", "5D"];
+        expectMultiplesToBe(play, [["3S", "3H", "3C", "3D"]]);
+    });
+
+    test('returns only one quad if in hand and cut card', () => {
+        var play = ["3S 3H 3C 5D", "3D"];
+        expectMultiplesToBe(play, [["3S", "3H", "3C", "3D"]]);
+    });
+
+});
