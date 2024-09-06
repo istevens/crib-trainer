0776ce4d614484718b3585e6eb960aff36634b54
Ian Stevens <istevens@null.net>
2024-09-06 11:11:08 -0400

    Changed internal hand representation to array of string as existing implementation had bugs and was difficult to follow. Added some new tests for runs as some behaviour was missing.

--- src/cribbagescorer.js
+++ src/cribbagescorer.js
@@ -3,88 +3,84 @@
 const JACK = 'J';
 const FACES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', JACK, 'Q', 'K'];
 const SUITS = ['D', 'C', 'H', 'S'];
-const fv = (a) => FACES.indexOf(a[0]);
-const cardCompare = (a,b) => fv(a) < fv(b) && -1 || fv(a) > fv(b) && 1 || 0;
+const fv = a => !a && -1 || FACES.indexOf(a.slice(0, -1));
+const cardCompare = (a, b) => fv(a) < fv(b) && -1 || fv(a) > fv(b) && 1 || 0;
 const extendExistingOrCreate = (m,v) => m.set(v[0], (m.get(v[0]) || []).concat(v[1]));
 
 export class CribbageHand {
 
     constructor(cards) {
-        cards = cards.map((x) => [x[0], [x[1]]])
-        this.cards = cards.reduce(extendExistingOrCreate, new Map());
+        this.cards = cards;
     }
 
     static fromString(s) {
         var hand = s.split(' ');
-        hand = hand.map((x) => [x.slice(0,-1), x.slice(-1)]);
         hand = new CribbageHand(hand);
         return hand;
     }
 
     hasHisNobs(cutCard) {
         var cutSuit = cutCard[1];
-        var jacks = this.cards.get(JACK) || false;
-        var jackOfCutSuitIsInHand = jacks && jacks.indexOf(cutSuit) >= 0;
+        var jackOfCutSuitIsInHand = this.cards.includes(JACK + cutSuit);
         return jackOfCutSuitIsInHand;
     }
 
     hasFlush() {
-        var suits = Array.from(this.cards.values()).flat();
-        suits = new Set(suits).size;
-        var hasFlush = suits == 1;
+        var suits = this.cards.map(x => x.slice(-1));
+        var hasFlush = new Set(suits).size == 1;
         return hasFlush;
     }
 
-    _includeCutCardWithHand(hand, cutCard) {
-        var hand = new Map(this.cards);
-        extendExistingOrCreate(hand, cutCard);
-        hand = Array.from(hand);
+    _includeCutCardWithHand(cutCard) {
+        var hand = this.cards.slice();
+        hand = hand.concat(cutCard);
         return hand;
     }
 
-    findMultiples(cutCard) {
-        var hand = this._includeCutCardWithHand(this.cards, cutCard);
-        var multiples = hand.filter((x) => x[1].length > 1);
-        multiples = multiples.map((x) => x[1].map((y) => x[0] + y));
+    _completeAndSortHand(cutCard) {
+        var hand = this._includeCutCardWithHand(cutCard);
+        hand = hand.toSorted(cardCompare);
+        return hand;
+    }
+
+    _groupHandAndCutCardByFaceValue(cutCard) {
+        var hand = this._completeAndSortHand(cutCard);
+        var addCardToExisting = (a, card) => cardCompare(a.at(-1).at(-1), card) == 0 && a.slice(0,-1).concat([a.at(-1).concat([card])]);
+        var addCardToMultiple = (a, card) => addCardToExisting(a, card) || a.concat([[card]]);
+        var multiples = hand.reduce(addCardToMultiple, [[]]);
+        multiples = multiples.slice(1);
         return multiples;
     }
 
-    _completeAndSortHand(cards, cutCard, reverse=false) {
-        var hand = this._includeCutCardWithHand(cards, cutCard);
-        hand = hand.toSorted(cardCompare);
-        hand = reverse && hand.toReversed() || hand;
-        return hand;
+    findMultiples(cutCard) {
+        var multiples = this._groupHandAndCutCardByFaceValue(cutCard);
+        multiples = multiples.filter(x => x.length > 1);
+        return multiples;
     }
 
     findRuns(cutCard) {
-        var hand = this._completeAndSortHand(this.cards, cutCard);
+        var hand = this._groupHandAndCutCardByFaceValue(cutCard);
 
-        var isInRun = (a, x) => a.length == 0 || fv(a.at(-1)) + 1 == fv(x);
-        var addIfInRun = (a, x) => isInRun(a, x) && a.concat([x]) || a;
-        var resetRun = (a) => (a.length < 3 && []);
-        var combineFaceAndSuits = (x) => x[1].map((y) => x[0]+y);
-        var runs = hand.reduce((a, x) => addIfInRun(a, x) || resetRun(a), []);
-        runs = runs.map(combineFaceAndSuits);
-
-        var addToNewRun = (runs, cards) => runs.length == 0 && [[cards]];
-        var addCardToRuns = (runs, card) => runs.map((y) => y.concat(card));
-        var combineRuns = (runs, cards) =>
-            cards.flatMap((card) =>
-                addToNewRun(runs, card) || addCardToRuns(runs, card)
-            );
-        runs = runs.reduce(combineRuns, []);
+        var collateRuns = (runs, card) => {
+            var addedToRun = false;
+            var cardFollowsRun = r => r.slice(-1).every(x => 1 + fv(x) == fv(card[0]));
+            var addToRun = (c, r) => {addedToRun = true; return c.map(x => r.concat(x));};
+            var newRuns = runs.flatMap(r => cardFollowsRun(r) && addToRun(card, r) || [r]);
+            newRuns = addedToRun && newRuns || newRuns.concat(addToRun(card, []));
+            return newRuns;
+        }
+        var runs = hand.reduce(collateRuns, [[]]);
+        runs = runs.filter(r => r.length > 2);
         return runs;
     }
 
     findFifteens(cutCard) {
-        var hand = this._completeAndSortHand(this.cards, cutCard, true);
-        var combineFaceAndSuits = (x) => x[1].map((y) => x[0]+y);
-        hand = hand.flatMap(combineFaceAndSuits);
+        var hand = this._completeAndSortHand(cutCard);
 
         var addCardToAll = (a, v) => a.concat(a.map(x => [v].concat(x)));
-        var cardScore = (x) => Math.min(10, 1 + FACES.indexOf(x[0]));
-        var handTotal = (a) => a.reduce((x, y) => x + cardScore(y), 0);
-        var cardsAddToFifteen = (a) => handTotal(a) == 15;
+        var cardScore = x => Math.min(10, 1 + FACES.indexOf(x[0]));
+        var handTotal = a => a.reduce((x, y) => x + cardScore(y), 0);
+        var cardsAddToFifteen = a => handTotal(a) == 15;
         var fifteens = hand.reduce(addCardToAll, [[]]);
         fifteens = fifteens.filter(cardsAddToFifteen);
 
--- src/cribbagescorer.test.js
+++ src/cribbagescorer.test.js
@@ -5,19 +5,17 @@ import { CribbageHand } from './cribbagescorer.js';
 describe('testing Hand.fromString', () => {
     const expectHandsEqual = (v, e) => {
         v = CribbageHand.fromString(v).cards;
-        v = Array.from(v.entries());
-        e = Object.entries(e);
         expect(v).toEqual(expect.arrayContaining(e));
     };
 
     test('returns hand matching string', () => {
         var hand = "AC 2C JD 4C";
-        expectHandsEqual(hand, {'A': ['C'], '2': ['C'], 'J': ['D'], '4': ['C']});
+        expectHandsEqual(hand, ['AC', '2C', 'JD', '4C']);
     });
 
     test('returns hand matching string with 10', () => {
         var hand = "AC 2C JD 10C";
-        expectHandsEqual(hand, {'A': ['C'], '2': ['C'], 'J': ['D'], '10': ['C']});
+        expectHandsEqual(hand, ['AC', '2C', 'JD', '10C']);
     });
 });
 
@@ -25,8 +23,8 @@ describe('testing Hand.hasHisNobs', () => {
     const expectHasNobs = (p, e) => {
         var [h, c] = p;
         h = CribbageHand.fromString(h);
-        var hasNobs = h.hasHisNobs(c);
-        expect(hasNobs).toBe(e);
+        var hasHisNobs = h.hasHisNobs(c);
+        expect(hasHisNobs).toBe(e);
     }
 
     test('returns true if suit of jack in hand matches cutCard', () => {
@@ -145,14 +143,23 @@ describe('testing Hand.findRuns', () => {
         expect(m).toEqual(e);
     }
 
+    test('returns zero runs if none in hand', () => {
+        var play = ["AC 3C 5C 7D", "9D"];
+        expectRunsToBe(play, []);
+    });
+
     test('returns only one run of three if in hand', () => {
         var play = ["AC 2C 3C 6D", "5D"];
         expectRunsToBe(play, [["AC", "2C", "3C"]]);
+        play = ["6D 4C 2C 5D", "AD"];
+        expectRunsToBe(play, [["4C", "5D", "6D"]]);
     });
 
     test('returns only one run of three if in hand and cut card', () => {
         var play = ["2C AC 5C 6D", "3C"];
         expectRunsToBe(play, [["AC", "2C", "3C"]]);
+        play = ["2C AC 5C 4D", "6D"];
+        expectRunsToBe(play, [["4D", "5C", "6D"]]);
     });
 
     test('returns two runs of three if middle card repeats', () => {
@@ -161,6 +168,12 @@ describe('testing Hand.findRuns', () => {
             play, [
                 ["AC", "2C", "3C"],
                 ["AC", "2D", "3C"]]
+        );
+        play = ["4C AC 3C 4D", "5D"];
+        expectRunsToBe(
+            play, [
+                ["3C", "4C", "5D"],
+                ["3C", "4D", "5D"]]
         );
     });
 
@@ -171,6 +184,12 @@ describe('testing Hand.findRuns', () => {
                 ["AC", "2C", "3C"],
                 ["AD", "2C", "3C"]]
         );
+        play = ["4C AC 3C 3D", "5D"];
+        expectRunsToBe(
+            play, [
+                ["3C", "4C", "5D"],
+                ["3D", "4C", "5D"]]
+        );
     });
 
     test('returns two runs of three if last card repeats', () => {
@@ -180,6 +199,12 @@ describe('testing Hand.findRuns', () => {
                 ["AC", "2C", "3C"],
                 ["AC", "2C", "3D"]]
         );
+        play = ["4C AC 5C 3D", "5D"];
+        expectRunsToBe(
+            play, [
+                ["3D", "4C", "5C"],
+                ["3D", "4C", "5D"]]
+        );
     });
 
     test('returns four runs of three if two cards repeat', () => {
@@ -187,8 +212,8 @@ describe('testing Hand.findRuns', () => {
         expectRunsToBe(
             play, [
                 ["AC", "2C", "3C"],
-                ["AC", "2D", "3C"],
                 ["AC", "2C", "3D"],
+                ["AC", "2D", "3C"],
                 ["AC", "2D", "3D"]]
         );
     });
@@ -305,7 +330,6 @@ describe('testing Hand.findFifteens', () => {
 
     test('returns one fifteen with four threes and a six', () => {
         var play = ["3C 6S 3S 3D", "3H"];
-        var fifteens = findFifteens(play);
         expectFifteensToBe(play, [
             ["3C", "6S", "3S", "3D"],
             ["6S", "3S", "3D", "3H"],
