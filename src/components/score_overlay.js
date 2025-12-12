e9e681ccc8d7dca19979034c1759c2303f7966e9
Ian Stevens <istevens@null.net>
2025-12-11 20:26:22 -0500

    Decorated score-overlay with checkmarks or crosses, depending on correct or incorrect, hopefully making it more obvious, and works better for colourblind.

--- src/components/score_overlay.js
+++ src/components/score_overlay.js
@@ -2,19 +2,18 @@ import * as Constants from "../constants.js";
 import defineComponent from "./SimpleTemplateComponent.js";
 
 defineComponent('score-overlay',
-    '${selectedScore}',
+    '<div>${selectedScore}</div>',
     `
     :host {
         display: flex;
         position: fixed;
         visibility: hidden;
-        justify-content: center;
-        align-items: center;
         width: 100%;
         height: 100%;
-        letter-spacing: -0.1em;
-        text-indent: -0.1em;
         filter: blur(0);
+        container-type: inline-size;
+        font-weight: 700;
+        font-size: clamp(11em, 55vw, 24em);
     }
 
     :host(.active) {
@@ -23,13 +22,44 @@ defineComponent('score-overlay',
         filter: blur(0.25em);
     }
 
-    :host(.correct) {
-        color: var(--successColour);
+    :host div {
+        flex: 1;
+        display: grid;
+        grid-template-columns: 1fr auto 1fr;
+        grid-auto-flow: column;
+        gap: 0.1em;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        letter-spacing: -0.1em;
+        margin-right: 0.1em;
     }
 
-    :host(.incorrect) {
-        color: var(--failureColour);
-    }`,
+    :host div::before,
+    :host div::after {
+        font-size: 50%;
+    }
+
+    :host div::before {
+        justify-self: flex-end;
+    }
+
+    :host div::after {
+        justify-self: flex-start;
+    }
+
+    :host(.correct) div { color: var(--successColour); }
+    :host(.incorrect) div { color: var(--failureColour); }
+
+    :host(.correct) div::before,
+    :host(.correct) div::after {
+        content: "\\2713";
+    }
+
+    :host(.incorrect) div::before,
+    :host(.incorrect) div::after {
+        content: "\\2717";
+    } `,
 
     function(ev) {
         const scoresMatch = ev.detail.scoresMatch;
